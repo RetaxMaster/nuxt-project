be77ba674bad6707e450652a1cb253c4776038c4
retaxmaster <retaxmaster@hotmail.com>
2021-05-31 01:36:34 -0500

    Adicion de meta informacion a las paginas

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Hola :3</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, totam obcaecati reiciendis aliquid tempora cumque dolorem mollitia dolores odit veritatis, fugit voluptatum eligendi iure corrupti. Blanditiis repellat aliquam eveniet nam.</p>
+    </div>
+</template>
+
+<script>
+export default {
+  
+  head() {
+    return {
+
+      title: "About - Posts",
+
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Ejemplo de sitio de posts con Nuxt"
+        },
+      ],
+
+      htmlAttrs: {
+        lang: "es"
+      }
+
+    }
+  }
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>¡Una app con Nuxt!</h1>
+</template>
+
+<script>
+export default {
+  
+  head() {
+    return {
+
+      title: "Home - Posts",
+
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Ejemplo de sitio de posts con Nuxt"
+        },
+      ],
+
+      htmlAttrs: {
+        lang: "es"
+      }
+
+    }
+  }
+
+}
+</script>
